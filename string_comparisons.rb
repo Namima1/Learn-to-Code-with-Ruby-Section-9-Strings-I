1ddc0c73fbc1c241989174f7cb5ba5c46c60c411
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-08-22 10:37:58 -0400

    Add files via upload

---
+++ string_comparisons.rb
@@ -0,0 +1,28 @@
+a = "Hello"
+b = "hello"
+c = "Hello"
+
+puts a == b 
+puts a == c
+puts a == a
+puts b == "hello"
+
+#== is asking if they're equal to one another 
+
+puts a != b
+puts a != c 
+
+#!= is inequality operator. It's asking if they're not equal to one another
+
+puts "A" < "B"
+puts "A" < "Z"
+puts "M" < "E"
+
+#< is asking if the letter on the left comes before the letter on the right 
+
+puts "L" > "F"
+puts "L" > "M"
+
+#> is asking if the letter on the left comes after the letter on the right  
+
+# ==, !=, <, > are operators we use to compare strings 
