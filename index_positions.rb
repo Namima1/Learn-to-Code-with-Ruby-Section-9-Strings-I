ba37be8f669add2d98315f660e995fb06fb37f03
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-08-27 14:55:24 -0400

    Add files via upload

---
+++ index_positions.rb
@@ -0,0 +1,16 @@
+story = "Once upon a time"
+#Ruby assigns characters a position in line, which is called the index position
+#ruby counts from 0
+#example: O is 0 
+#bc it counts from 0 the index position will always be one less than the .length
+
+#How to use index positions to extract a specific character from string 
+#If I wanted O
+
+puts story[0]
+#Use square brackets to get a certain position we want 
+#[0] refers to the first character, which is O
+
+#If I want a letter from the end I input a negative number 
+puts story [-2]
+#this will give me m because it is second to last 
