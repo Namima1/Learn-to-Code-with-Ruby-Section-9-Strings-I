7a5f072e29c1a664bbb9e35269f32286cbcb8ad4
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-08-30 10:32:38 -0400

    Add files via upload

---
+++ extract_multiple_characters_from_string.rb
@@ -0,0 +1,13 @@
+story = "Once upon a time in a land far, far away..."
+
+puts story[5, 4]
+#this is saying to start at index position 5, which will be "u" in upon
+#and take 4 character which will be the whole word upon 
+
+p story [0, 5]
+#the 0 is starting from the beginning
+#the p pulls the whole word in quotes
+
+puts story[0, story.length]
+
+#adding story.length after the 0 pulls the whole string 
