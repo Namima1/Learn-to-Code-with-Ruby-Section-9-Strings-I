6cec805d0b08b829e6be0b0bbba5f36436cb25c5
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-02 13:46:13 -0400

    Add files via upload

---
+++ the_insert_method_practice.rb
@@ -0,0 +1,10 @@
+#insert method inserts or adds a character without replacing existing character
+
+typo = "Georg Washington"
+
+#variable type is assigned to "GeorgWashington", the e is missing
+#we wan to add the e without taking anything out
+
+typo.insert(5, "e")
+#the 5 targets the position and the e says what to add 
+puts typo 
